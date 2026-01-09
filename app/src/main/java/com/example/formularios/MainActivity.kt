36adbf05e6228136cb49758439c2d9e44b26fa9d
Cristian <crisfernandoxdpatzifrias@gmail.com>
2026-01-09 20:29:18 +0100

    tercer commit

--- app/src/main/java/com/example/formularios/MainActivity.kt
+++ app/src/main/java/com/example/formularios/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.formularios
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -10,7 +11,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.common.data.DataHolder
 import kotlin.jvm.java
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var imgPerfil: ImageView
