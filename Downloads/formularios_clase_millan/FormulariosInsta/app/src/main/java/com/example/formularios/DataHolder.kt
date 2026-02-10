ed491e99ec248cc7e127857137224c088bed06e8
ELRACING <millan.delpon@salesianoslosboscos.com>
2026-02-10 23:24:52 +0100

    Cambios de clase 09/02/2026, presentacion de formularios y permisos

---
+++ Downloads/formularios_clase_millan/FormulariosInsta/app/src/main/java/com/example/formularios/DataHolder.kt
@@ -0,0 +1,12 @@
+package com.example.formularios
+
+import android.net.Uri
+
+object DataHolder {
+    var nombre: String = ""
+    var descripcion: String = ""
+    var genero: String = ""
+    var opcionSeleccionada: String = ""
+    var perfilPublico: Boolean = false
+    var fotoPerfil: Uri? = null
+}
